3bf28487029464b9ae1079485152c7e4745118bf
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-04-24 16:09:01 -0500

    Time: 65 ms (60.41%), Space: 15.1 MB (35.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         numDict = {}
         
         for i in range(len(nums)):
-            if (target - nums[i]) in numDict.keys():
+            if (target - nums[i]) in numDict:
                 return [i, numDict[(target - nums[i])]]
             numDict[nums[i]] = i
         
