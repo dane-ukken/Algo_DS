47901947981074fa0761d5016fe0b743a0de32e8
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-04-24 16:50:26 -0500

    Time: 59 ms (22.52%), Space: 14.6 MB (65.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,26 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
+        
+        i = 0
+        j = len(s) - 1
+        
+        while (i<j and i<len(s) and j>=0) :
+            if not s[i].isalnum():
+                i += 1
+                continue
+            if not s[j].isalnum():
+                j -= 1
+                continue
+            if s[i].lower()!=s[j].lower():
+                return False
+            else:
+                i += 1
+                j -= 1
+                
+        return True
+        
+        '''
         newStr = ""
         
         for c in s:
@@ -7,16 +28,15 @@ class Solution:
                 newStr += c.lower()
         
         return newStr == newStr[::-1]
-        
+        '''
         
         
         """single pass"""
         """
-        length = len(s)
         i = 0
         j = length - 1
         
-        while (i<j and i<length-1 and j>=0) :
+        while (i<j and i<len(s) and j>=0) :
             if not s[i].isalnum():
                 i += 1
                 continue
