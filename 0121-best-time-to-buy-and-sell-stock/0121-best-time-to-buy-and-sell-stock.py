2981beed8ed43f9d08c9b21d9a522ce817fd29fc
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-05-15 00:12:23 -0500

    Time: 1202 ms (6.00%), Space: 27.4 MB (18.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        maxPrices = [0]*n
         maxProfitVal = 0
+        maxPrices = [0]*n
         
         for i in range(n):
             if i == 0:
@@ -10,12 +10,8 @@ class Solution:
                 continue
             maxPrices[n-1-i] = max(maxPrices[n-i], prices[n-1-i])
         
-        maxProfitsList = [0]*n
-        
         for i in range(n):
-            maxProfitsList[i] = maxPrices[i]-prices[i]
-            if maxProfitsList[i] > maxProfitVal:
-                maxProfitVal = maxProfitsList[i]
+            maxProfitVal = max(maxPrices[i]-prices[i], maxProfitVal)
         
         return maxProfitVal
         
