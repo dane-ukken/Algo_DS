b062bc7d2dcf2f4020a3bf460267e376d5fcdac6
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-05-14 23:53:08 -0500

    Time: 1173 ms (7.64%), Space: 26.8 MB (29.43%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        maxPrices = [0]*n
+        maxProfitVal = 0
+        
+        for i in range(n):
+            if i == 0:
+                maxPrices[n-1-i] = prices[n-1-i]
+                continue
+            maxPrices[n-1-i] = max(maxPrices[n-i], prices[n-1-i])
+        
+        maxProfitsList = [0]*n
+        
+        for i in range(n):
+            maxProfitsList[i] = maxPrices[i]-prices[i]
+            if maxProfitsList[i] > maxProfitVal:
+                maxProfitVal = maxProfitsList[i]
+        
+        return maxProfitVal
+        
