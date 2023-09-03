cef39f56465e28a33dbd712f77e0dba75f4f09a5
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-09-03 15:43:55 -0500

    Time: 46 ms (82.70%), Space: 18.6 MB (68.93%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,12 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
     
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maxDiameter = [0]
+        
+        def getHeightAndSetDiameter(root):
+            if not root:
+                return -1
+            
+            leftHeight = getHeightAndSetDiameter(root.left)            
+            rightHeight = getHeightAndSetDiameter(root.right)
+
+            maxDiameter[0] = max(maxDiameter[0], 2 + leftHeight + rightHeight)
+            
+            return 1 + max(leftHeight, rightHeight)
+            
+        
+        getHeightAndSetDiameter(root)
+        return maxDiameter[0]
+            
+        
+        
+        '''
         if not root:
             return 0
         
@@ -19,3 +36,9 @@ class Solution:
         diameter = leftTreeDepth + rightTreeDepth
         
         return max(diameter, self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
+    
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    '''
