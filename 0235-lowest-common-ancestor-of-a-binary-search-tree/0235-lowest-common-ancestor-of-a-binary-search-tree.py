0e60fc6b8d3b68dd1c0f43679b832024cd5e07c1
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-09-25 13:39:56 -0500

    Time: 71 ms (68.05%), Space: 20.8 MB (50.90%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,6 +7,21 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        cur = root
+        
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
+    
+    
+    
+    """
+    
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if p.val < q.val:
             return self.lowestCommonAncestorSortedInputs(root, p, q)
         return self.lowestCommonAncestorSortedInputs(root, q, p)
@@ -17,3 +32,5 @@ class Solution:
                 return root
             return self.lowestCommonAncestorSortedInputs(root.left, p, q)
         return self.lowestCommonAncestorSortedInputs(root.right, p, q)
+        
+    """
