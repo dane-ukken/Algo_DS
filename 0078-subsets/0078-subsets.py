1124312637fc7546ff64a279ee10fcad4dabdd74
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2023-09-26 18:18:53 -0500

    Time: 48 ms (11.13%), Space: 16.3 MB (98.80%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,9 +3,16 @@ class Solution:
         result = []
         result.append([])
         for num in nums:
-            result = self.addToResult(result, num)  
+            #result = self.addToResult(result, num)
+            newSets = []
+            for currentSet in result:
+                newSets.append(currentSet[:])
+                currentSet.append(num)
+                newSets.append(currentSet[:])
+            result = newSets
         return result            
     
+    """
     def addToResult(self, resultSet: List[List[int]], num: int) -> List[List[int]]:
         newSets = []
         for currentSet in resultSet:
@@ -13,3 +20,4 @@ class Solution:
             currentSet.append(num)
             newSets.append(currentSet[:])
         return newSets
+    """
