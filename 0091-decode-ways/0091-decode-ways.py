6e0bac548e5d82436bfdc27ceb538661541eda66
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2024-01-01 20:56:48 -0600

    Time: 68 ms (5.01%), Space: 18 MB (7.47%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,36 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        codeDict = {}
-        for i in range(26):
-            if i+1 >= 11 and i+1 != 20:
-                codeDict[str(i+1)] = 2
-            else:
-                codeDict[str(i+1)] = 1
-        codeDict[""] = 0
-                
+        codeDict = {"": 1}  # Base case: one way to decode an empty string
+
         def findPossibleDecodeCount(s):
-            nonlocal codeDict
-            first, second = s[0:1], s[0:2]
-            firstTotal, secondTotal = 0, 0
-            
-            if not s:
-                return 0
-            
             if s in codeDict:
                 return codeDict[s]
-            
-            if first in codeDict:
-                findPossibleDecodeCount(s[1:])
-                firstTotal = codeDict[s[1:]]
-            
-            if second in codeDict and int(second) <= 26:
-                findPossibleDecodeCount(s[2:])
-                secondTotal = codeDict[s[2:]]
-                        
+
+            if s[0] == '0':  # Strings starting with '0' cannot be decoded
+                return 0
+
+            # Decode using the first character
+            firstTotal = findPossibleDecodeCount(s[1:])
+
+            # Decode using the first two characters if valid
+            secondTotal = 0
+            if len(s) >= 2 and s[0:2] <= "26":
+                secondTotal = findPossibleDecodeCount(s[2:])
+
             codeDict[s] = firstTotal + secondTotal
-        
-        findPossibleDecodeCount(s)
-        print(codeDict)
-        return codeDict[s]
-        
-        
+            return codeDict[s]
+
+        return findPossibleDecodeCount(s)
