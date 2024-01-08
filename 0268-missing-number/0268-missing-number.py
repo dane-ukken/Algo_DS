ab5e306e4cc127c7103638c54e9dbdfb24492e36
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2024-01-08 16:11:13 -0600

    Time: 104 ms (98.07%), Space: 18.5 MB (23.83%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        """
         n, s = len(nums), 0
         
         for i in range(n):
@@ -8,11 +9,10 @@ class Solution:
         return int(n*(n+1)/2 - s)
         
         """
-        res = nums[0]
-        for i in range(1, len(nums)):
-            res = res ^ i 
-            res = res ^ nums[i]
+        res = len(nums)
+        for i in range(len(nums)):
+            res ^= i ^ nums[i]
         
-        res = res ^ len(nums)
         return res
+        """
         """
