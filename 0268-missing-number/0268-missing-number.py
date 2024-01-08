da3c2dc4168a0a80fd90fc2bb6455c68a11ab814
ukkendane96 <70409664+ukkendane96@users.noreply.github.com>
2024-01-08 16:07:22 -0600

    Time: 110 ms (92.88%), Space: 18.6 MB (12.28%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        res = nums[0]
+        n, s = len(nums), 0
         
+        for i in range(n):
+            s += nums[i]
+        
+        return int(n*(n+1)/2 - s)
+        
+        """
+        res = nums[0]
         for i in range(1, len(nums)):
             res = res ^ i 
             res = res ^ nums[i]
         
         res = res ^ len(nums)
         return res
+        """
