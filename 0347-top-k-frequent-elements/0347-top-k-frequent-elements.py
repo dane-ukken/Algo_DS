16b1ac12e2b2bcebe48a3b104312f7e3068144f7
Dane Ukken <ukkendane@gmail.com>
2024-03-14 00:33:06 -0500

    Time: 89 ms (59.85%), Space: 21 MB (85.10%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         countDict = defaultdict(lambda: 0)
+        res = []
+        for n in nums:
+            countDict[n] += 1
+            
+        max_heap = []    
+        for (key, val) in countDict.items():
+            max_heap.append((-val, key))
+        heapq.heapify(max_heap)
+        
+        
+        while k > 0:
+            res.append(heapq.heappop(max_heap)[1])
+            k -= 1
+        
+        return res
+        
+        '''
+        countDict = defaultdict(lambda: 0)
         nBuckets = [[] for i in range(len(nums)+1)]
         res = []
         for n in nums:
@@ -13,3 +31,4 @@ class Solution:
                 if len(res) == k:
                     return res
         return res
+        '''
