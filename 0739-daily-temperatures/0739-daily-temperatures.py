f3d3cbda5bd3036bc92f6892257822b6866e07ae
Dane Ukken <ukkendane@gmail.com>
2024-03-15 18:15:16 -0500

    Time: 915 ms (33.68%), Space: 32.4 MB (13.95%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,5 +1,16 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0]*len(temperatures)
+        stack = [] # pair: (index, val)
+        
+        for i, t in enumerate(temperatures):
+            while len(stack) > 0 and t > stack[-1][1]:
+                index, val = stack.pop()
+                res[index] = i - index
+            stack.append([i, t])
+        return res
+        
+        '''
         res, stack = [0]*len(temperatures), []
         
         for i in range(len(temperatures)):
@@ -13,7 +24,7 @@ class Solution:
             stack.append(temperatures[i])
         
         return res
-                    
+        '''            
         
         
         
