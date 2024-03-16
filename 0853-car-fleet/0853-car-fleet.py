524e785a9298e333091918f3caef7be9093c1d6d
Dane Ukken <ukkendane@gmail.com>
2024-03-15 22:49:51 -0500

    Time: 684 ms (60.56%), Space: 39.2 MB (32.63%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,5 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        res = 0
+        pT = [(p, (target-p)/s) for p, s in zip(position,speed)]      
+        pT.sort()
+        while len(pT) > 0:
+            currTime = (pT.pop())[1]
+            res += 1
+            while len(pT) > 0 and currTime >= (pT[-1])[1]:
+                pT.pop()
+        return res
+        '''
         n = len(position)
         time = {}
         res = 0    
@@ -13,3 +23,4 @@ class Solution:
             while len(position) > 0 and currTime >= time[position[-1]]:
                 position.pop()
         return res
+        '''
