3306983feb6c1f34dad30f9f156eaf2a07e23f33
Dane Ukken <ukkendane@gmail.com>
2024-03-15 22:32:06 -0500

    Time: 654 ms (85.74%), Space: 42 MB (7.39%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -2,18 +2,14 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         n = len(position)
         time = {}
-        res = 0
-        
+        res = 0    
         for i in range(n):
             x = position[i]
-            time[x] = (target-x)/speed[i]
-        
+            time[x] = (target-x)/speed[i]        
         position.sort()
         while len(position) > 0:
             currTime = time[position.pop()]
             res += 1
             while len(position) > 0 and currTime >= time[position[-1]]:
                 position.pop()
-                
-        
         return res
