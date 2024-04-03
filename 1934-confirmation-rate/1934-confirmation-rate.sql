82d1fafeed04c2682765b2172a0f81fbd73bfbc1
Dane Ukken <ukkendane@gmail.com>
2024-04-03 00:06:11 -0500

    Time: 1324 ms (29.51%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,13 +1,25 @@
 /* Write your T-SQL query statement below */
 
+SELECT 
+    s.user_id, 
+    ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 2) AS confirmation_rate
+FROM 
+    Signups AS s 
+LEFT JOIN 
+    Confirmations AS c 
+ON 
+    s.user_id = c.user_id 
+GROUP BY 
+    s.user_id;
+
+/*
 select
     s.user_id,
     CASE
         WHEN SUM(t.confirmedCount) IS NULL
         THEN 0
         ELSE CAST(CAST(SUM(t.confirmedCount) AS float)/CAST(SUM(t.timeoutCount + t.confirmedCount) AS float) AS DECIMAL(10, 2))
-    END as confirmation_rate
-    
+    END as confirmation_rate   
 from
     Signups s
     left join
@@ -29,7 +41,7 @@ from
     ) t on t.user_id = s.user_id
 group by
     s.user_id
-
+*/
 /*
 select
     user_id,
