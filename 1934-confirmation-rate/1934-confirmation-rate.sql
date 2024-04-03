1b89fe157e2c46d49b133873740f76b30861d464
Dane Ukken <ukkendane@gmail.com>
2024-04-03 00:01:24 -0500

    Time: 1752 ms (9.30%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,87 @@
+/* Write your T-SQL query statement below */
+
+select
+    s.user_id,
+    CASE
+        WHEN SUM(t.confirmedCount) IS NULL
+        THEN 0
+        ELSE CAST(CAST(SUM(t.confirmedCount) AS float)/CAST(SUM(t.timeoutCount + t.confirmedCount) AS float) AS DECIMAL(10, 2))
+    END as confirmation_rate
+    
+from
+    Signups s
+    left join
+    (
+        select 
+            user_id,
+            CASE
+                WHEN action = 'confirmed'
+                THEN 1
+                ELSE 0
+            END as confirmedCount,
+            CASE
+                WHEN action = 'timeout'
+                THEN 1
+                ELSE 0
+            END as timeoutCount
+        from
+            Confirmations
+    ) t on t.user_id = s.user_id
+group by
+    s.user_id
+
+/*
+select
+    user_id,
+    SUM(confirmedCount),
+    SUM(timeoutCount)
+from
+(
+    select 
+        user_id,
+        CASE
+            WHEN action = 'confirmed'
+            THEN 1
+            ELSE 0
+        END as confirmedCount,
+        CASE
+            WHEN action = 'timeout'
+            THEN 1
+            ELSE 0
+        END as timeoutCount
+    from
+        Confirmations
+
+) t
+group by 
+    user_id
+*/
+
+/*
+
+    select
+        s.user_id,
+        SUM(t.confirmedCount)/SUM(t.timeoutCount + t.confirmedCount) as confirmation_rate
+    from
+        Signups s
+        join
+        (
+            select 
+                user_id,
+                CASE
+                    WHEN action = 'confirmed'
+                    THEN 1
+                    ELSE 0
+                END as confirmedCount,
+                CASE
+                    WHEN action = 'timeout'
+                    THEN 1
+                    ELSE 0
+                END as timeoutCount
+            from
+                Confirmations
+        ) t on t.user_id = s.user_id
+    group by
+        s.user_id
+
+*/
