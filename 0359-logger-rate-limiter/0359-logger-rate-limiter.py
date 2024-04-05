129c04b1bab159ac97231b850f5b60ebd56f63e5
Dane Ukken <ukkendane@gmail.com>
2024-04-04 21:06:04 -0500

    Time: 117 ms (70.62%), Space: 23.2 MB (60.94%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -1,16 +1,32 @@
-class Logger:
+from collections import deque
+
+class Logger(object):
 
     def __init__(self):
-        self.messageMap = {};
-
-    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        if message in self.messageMap and self.messageMap[message] + 10 > timestamp:
-            return False
-            
-        self.messageMap[message] = timestamp
-        return True
+        """
+        Initialize your data structure here.
+        """
+        self._msg_set = set()
+        self._msg_queue = deque()
+    
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        """
+        while self._msg_queue:
+            msg, ts = self._msg_queue[0]
+            if timestamp - ts >= 10:
+                self._msg_queue.popleft()
+                self._msg_set.remove(msg)
+            else:
+                break
         
-
+        if message not in self._msg_set:
+            self._msg_set.add(message)
+            self._msg_queue.append((message, timestamp))
+            return True
+        else:
+            return False
 
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
