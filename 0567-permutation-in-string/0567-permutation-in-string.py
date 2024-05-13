88b2179dd73c707bad52247e6b0c0229cb20e60c
Dane Ukken <ukkendane@gmail.com>
2024-05-12 22:32:40 -0500

    Time: 84 ms (40.56%), Space: 16.7 MB (78.54%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        winLen = len(s1)
+        res = False
+        if len(s2) < winLen:
+            return res
+        
+        charCount = Counter(s1)
+        #print(charCount)
+        
+        l, r = 0, 0
+        charDict = defaultdict(lambda: 0)
+        while r < len(s2):
+            if s2[r] not in charCount:
+                l, r = r+1, r+1
+                charDict = defaultdict(lambda: 0)
+                continue
+            
+            #if r - l + 1 <= winLen:
+            charDict[s2[r]] += 1
+                
+            #print(r, l, charDict)
+            if r - l + 1 == winLen:
+                flag = True
+                for key in charCount.keys():
+                    if charCount[key] != charDict[key]:
+                        flag = False
+                if flag:
+                    return True
+                charDict[s2[l]] -= 1
+                l += 1
+            r += 1
+                                
+            
+            
+        
+        return res
