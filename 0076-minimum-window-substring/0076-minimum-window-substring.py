e84578e11e4450e8f31e254b929be7902d6d4ef2
Dane Ukken <ukkendane@gmail.com>
2024-05-13 04:05:15 -0500

    Time: 109 ms (61.93%), Space: 17.3 MB (80.33%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,9 +2,9 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         sCount = defaultdict(int)        
         tCount = defaultdict(int)
-        res = [len(s), 0, ""]
+        res = [len(s), 0]
         for x in t: tCount[x] += 1
-        
+        hasFound = False
         l, r = 0, 0
         need, have = len(tCount.keys()), 0
         while r < len(s):
@@ -15,9 +15,9 @@ class Solution:
                     have += 1
 
             while need == have:
-                #print(res)
                 if res[0] - res[1] + 1 > r - l + 1:
-                    res = [r, l, s[l:r+1]]
+                    res = [r, l]
+                    hasFound = True
                 lCurr = s[l]
                 if lCurr in tCount:
                     sCount[lCurr] -= 1
@@ -26,4 +26,6 @@ class Solution:
                 l += 1
             r += 1
 
-        return res[2]
+        if not hasFound:
+            return ""
+        return s[res[1]:res[0]+1]
