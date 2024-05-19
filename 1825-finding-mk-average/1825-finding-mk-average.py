a4b228abdb72221857ea05183781fd43ad9ea08f
Dane Ukken <ukkendane@gmail.com>
2024-05-19 17:53:14 -0500

    Time: 873 ms (66.62%), Space: 52.1 MB (73.79%) - LeetHub

--- 1825-finding-mk-average/1825-finding-mk-average.py
+++ 1825-finding-mk-average/1825-finding-mk-average.py
@@ -1,62 +1,42 @@
 from sortedcontainers import SortedList
+
 class MKAverage:
 
-    MAX_NUM = 10 ** 5
     def __init__(self, m: int, k: int):
-        
-        self.m = m
-        self.k = k
-        
-        # sorted list
-        self.sl = SortedList([0] * m)
-		# sum of k smallest elements
-        self.sum_k = 0
-		# sum of m - k smallest elements
-        self.sum_m_k = 0
-        
-        # queue for the last M elements if the stream
-        self.q = deque([0] * m)
-        
+        self.m, self.k = m, k
+        self.deque = collections.deque()
+        self.sl = SortedList()
+        self.total = self.first_k = self.last_k = 0
+
     def addElement(self, num: int) -> None:
-        # Time: O(logm)
-		
-        m, k, q, sl = self.m, self.k, self.q, self.sl            
-            
-        # update q
-        q.append(num)
-        old = q.popleft()
-        
-        # remove the old num
-        r = sl.bisect_right(old)
-		# maintain sum_k
-        if r <= k:
-            self.sum_k -= old
-            self.sum_k += sl[k]
-		# maintain sum_m_k
-        if r <= m - k:
-            self.sum_m_k -= old
-            self.sum_m_k += sl[m-k]
-        # remove the old num
-        sl.remove(old)
-        
-        # add the new num
-        r = sl.bisect_right(num)
-        if r < k:
-            self.sum_k -= sl[k-1]
-            self.sum_k += num
-        if r < m - k:
-            self.sum_m_k -= sl[m - k - 1]
-            self.sum_m_k += num
-        
-        sl.add(num)
-            
-        return
+        self.total += num
+        self.deque.append(num)
+        index = self.sl.bisect_left(num)
+        if index < self.k:
+            self.first_k += num
+            if len(self.sl) >= self.k:
+                self.first_k -= self.sl[self.k - 1]
+        if index >= len(self.sl) + 1 - self.k:
+            self.last_k += num
+            if len(self.sl) >= self.k:
+                self.last_k -= self.sl[-self.k]
+        self.sl.add(num)
+        if len(self.deque) > self.m:
+            num = self.deque.popleft()
+            self.total -= num
+            index = self.sl.index(num)
+            if index < self.k:
+                self.first_k -= num
+                self.first_k += self.sl[self.k]
+            elif index >= len(self.sl) - self.k:
+                self.last_k -= num
+                self.last_k += self.sl[-self.k - 1]
+            self.sl.remove(num)
 
     def calculateMKAverage(self) -> int:
-		# Time: O(1)
-        if self.sl[0] == 0:
+        if len(self.sl) < self.m:
             return -1
-        return (self.sum_m_k - self.sum_k) // (self.m - self.k * 2)
+        return (self.total - self.first_k - self.last_k) // (self.m - 2 * self.k)
 
 
 # Your MKAverage object will be instantiated and called as such:
