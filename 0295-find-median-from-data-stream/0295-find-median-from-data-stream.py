9bed0bf322de19226f90636c560a755f929e1a7d
Dane Ukken <ukkendane@gmail.com>
2024-06-06 23:04:55 -0500

    Time: 376 ms (51.25%), Space: 38.6 MB (31.90%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,20 +1,39 @@
 class MedianFinder:
 
     def __init__(self):
-        self.stream = []
-
-    def addNum(self, num: int) -> None:
-        left = bisect.bisect_left(self.stream, num)
+        self.low = [] # maxheap
+        self.high = [] # minheap
+        heapq.heapify(self.low)
+        heapq.heapify(self.high)
         
-        self.stream[left: left] = [num]
+    def addNum(self, num: int) -> None:
+        low, high = self.low, self.high
+        
+        heapq.heappush(low, -num)
+
+        if low and high and -low[0] > high[0]:
+            curr = -1 * heapq.heappop(low)
+            heapq.heappush(high, curr)
+        
+        if len(low) > len(high) + 1:
+            curr = -1 * heapq.heappop(low)
+            heapq.heappush(high, curr)
+        
+        if len(high) > len(low) + 1:
+            curr = heapq.heappop(high)
+            heapq.heappush(low, -curr)
+        
 
     def findMedian(self) -> float:
+        low, high = self.low, self.high
 
-        n = len(self.stream)
-        if n % 2 == 0:
-            return (self.stream[n//2] + self.stream[n//2 - 1]) / 2
+        if len(low) > len(high):
+            return float(-low[0])
+        elif len(low) < len(high):
+            return float(high[0])
         else:
-            return self.stream[n//2]
+            return (high[0]-low[0])/2
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:
