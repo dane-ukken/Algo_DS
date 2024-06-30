9437f98c770321eb8afa1d7f6f0054baeefcc9d1
Dane Ukken <ukkendane@gmail.com>
2024-06-30 14:05:43 -0500

    Time: 558 ms (23.56%), Space: 24.4 MB (40.90%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,27 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        visited, unsafeNodes = set(), set()
+        edgeDict = defaultdict(lambda: [])
+        cycle = set()
+        
+        def dfs(node):
+            if node in cycle or node in unsafeNodes:
+                unsafeNodes.update(cycle)
+                return
+            if node in visited:
+                return
+            cycle.add(node)
+            
+            for des in graph[node]:
+                dfs(des)
+            
+            cycle.remove(node)
+            visited.add(node)
+            
+        for i in range(len(graph)):
+            dfs(i)
+
+        res = visited.difference(unsafeNodes)
+        res = list(res)
+        res.sort()
+        return res
