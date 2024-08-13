624a3864f646377f65c7add0bc7ce127f6df4dfd
Dane Ukken <ukkendane@gmail.com>
2024-08-13 17:03:17 -0500

    Time: 23 ms (39.53%), Space: 11.8 MB (19.66%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,12 +1,25 @@
-class Solution:
-    def partitionLabels(self, s: str) -> List[int]:
-        last = {c: i for i, c in enumerate(s)}
-        j = anchor = 0
-        ans = []
+class Solution(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        # count the last occurrence
+        
+        # split to new if last occurrence is the curr
+        
+        charLastDict = defaultdict(int)
         for i, c in enumerate(s):
-            j = max(j, last[c])
-            if i == j:
-                ans.append(i - anchor + 1)
-                anchor = i + 1
+            charLastDict[c] = i
+        
+        res = []
+        st, e = 0, 0
+        for i, c in enumerate(s):
+            e = max(e, charLastDict[c])
+            if i == e:
+                res.append(e - st + 1)
+                st = i+1
+        
+        return res
             
-        return ans
+            
