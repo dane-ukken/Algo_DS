a67b29e45cded6d4009b44d5dd160286bc30e31c
Dane Ukken <ukkendane@gmail.com>
2024-08-15 14:36:50 -0500

    Time: 23 ms, Memory: 12.4 MB - LeetHub

--- binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -24,7 +24,7 @@ class Solution(object):
                     q.append(curr.left)
                 if curr.right:
                     q.append(curr.right)
-            res.append(currList[:])
+            res.append(currList)
                 
             
             
