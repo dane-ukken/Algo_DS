df487ef1d5fad07ef8a09af604b1174319085b9f
Dane Ukken <ukkendane@gmail.com>
2024-10-25 04:29:35 -0500

    Time: 118 ms (95.4%), Space: 16.6 MB (60.09%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,20 +1,19 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        palSubCount, n = 0, len(s)
-        
-        def addNumberOfPalSubCenteredAtI(i, evenVal):
-            nonlocal n, s, palSubCount
-            l, r = i, i+evenVal
-            
-            while l >= 0 and r < n and s[l]==s[r]:
-                palSubCount += 1
-                r += 1
+        def countPals(l, r):
+            count = 0
+            while l >= 0 and r < len(s):
+                if s[r] != s[l]:
+                    break
+                count += 1
                 l -= 1
+                r += 1
+            
+            return count
         
-        for i in range(n):
-            addNumberOfPalSubCenteredAtI(i, 0)
-            addNumberOfPalSubCenteredAtI(i, 1)
-        
-        return palSubCount
-        
-        
+        res = 0
+        for idx, c in enumerate(s):
+            res += countPals(idx, idx)
+            res += countPals(idx, idx+1)
+
+        return res
