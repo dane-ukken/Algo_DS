2cfdbf4c14dfbccc83bf68b8d08a361ed7d51390
Dane Ukken <ukkendane@gmail.com>
2025-08-24 03:13:43 -0700

    Time: 3 ms (31.34%), Space: 17.7 MB (94.01%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.py
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -1,40 +1,62 @@
 class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
-        edits = 0
-        
-        if len(s) == len(t) - 1:
-            if not t:
-                return True
-            idx = 0
-            while idx < len(s):
-                if edits > 1:
-                    return False
-                if s[idx] != t[idx + edits]:
-                    edits += 1
-                else:
-                    idx += 1
-        
-        elif len(s) == len(t) + 1:
-            if not s:
-                return True
-            idx = 0
-            while idx < len(t):
-                if edits > 1:
-                    return False
-                if t[idx] != s[idx + edits]:
-                    edits += 1
-                else:
-                    idx += 1
-        
-        elif len(s) == len(t):
-            for i in range(len(s)):
-                if s[i] != t[i]:
-                    edits += 1
-                    if edits > 1:
-                        return False
-            if edits != 1:
-                return False
-        else:
+        ns, nt = len(s), len(t)
+
+        if ns > nt:
+            return self.isOneEditDistance(t, s)
+
+        if nt - ns > 1:
             return False
+
+        for i in range(ns):
+            if s[i] != t[i]:
+                if ns == nt:
+                    return s[i + 1 :] == t[i + 1 :]
+                else:
+                    return s[i:] == t[i + 1 :]
+
+        # If there are no diffs in ns distance
+        # The strings are one edit away only if
+        # t has one more character.
+        return ns + 1 == nt
+
+
+
+        # edits = 0
+        
+        # if len(s) == len(t) - 1:
+        #     if not t:
+        #         return True
+        #     idx = 0
+        #     while idx < len(s):
+        #         if edits > 1:
+        #             return False
+        #         if s[idx] != t[idx + edits]:
+        #             edits += 1
+        #         else:
+        #             idx += 1
+        
+        # elif len(s) == len(t) + 1:
+        #     if not s:
+        #         return True
+        #     idx = 0
+        #     while idx < len(t):
+        #         if edits > 1:
+        #             return False
+        #         if t[idx] != s[idx + edits]:
+        #             edits += 1
+        #         else:
+        #             idx += 1
+        
+        # elif len(s) == len(t):
+        #     for i in range(len(s)):
+        #         if s[i] != t[i]:
+        #             edits += 1
+        #             if edits > 1:
+        #                 return False
+        #     if edits != 1:
+        #         return False
+        # else:
+        #     return False
             
-        return True
+        # return True
