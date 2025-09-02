f5e89845841cac8b7b30f22736063e9b4fc2a218
Dane Ukken <ukkendane@gmail.com>
2025-09-01 19:36:24 -0700

    Time: 1414 ms (5.1%), Space: 63.3 MB (5.44%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        visited = defaultdict(int)
+        neighborDict = defaultdict(list)
+        minHeap = []
+
+        for s, d, p in flights:
+            neighborDict[s].append((p, d))
+
+        heapq.heappush(minHeap, (0, -1, src))
+        while minHeap:
+            l = len(minHeap)
+            for i in range(l):
+                p, stops, node = heapq.heappop(minHeap)
+                print(p, stops, node)
+                if visited[node] > 2*k:
+                    continue
+                visited[node] += 1
+                if node == dst:
+                    if stops > k:
+                        visited[node] = 0
+                        continue
+                    else:
+                        return p
+                for dp, neighbor in neighborDict[node]:
+                    newPrice = p + dp
+                    heapq.heappush(minHeap, (newPrice, stops + 1, neighbor))
+        
+        return -1        
